350204e11f79f7754dce6e878d60a1d979efc087
ankurrrrr7 <ankurjha956@gmail.com>
2025-01-11 23:45:32 +0530

    example

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -10,17 +10,7 @@ const ALL_USERS = [
       username: "harkirat@gmail.com",
       password: "123",
       name: "harkirat singh",
-    },
-    {
-      username: "raman@gmail.com",
-      password: "123321",
-      name: "Raman singh",
-    },
-    {
-      username: "priya@gmail.com",
-      password: "123321",
-      name: "Priya kumari",
-    },
+    }
   ];
 app.post("/signin",(req, res)=>{
     const username = req.body.username;
